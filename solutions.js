6ec00e658fd943443f8e85fcc5f5a6a0d07c94a4
Christian Larsen <christian.p.larsen.94@gmail.com>
2021-06-02 08:39:47 -0500

    Initial push

--- solutions.js
+++ solutions.js
@@ -20,3 +20,46 @@
  * and
  * concat
  */
+
+function isTrue(valueT) {
+    return valueT === true;
+}
+
+function isFalse(valueF) {
+    return valueF === false;
+}
+
+function not(valueNot1, valueNot2) {
+    return valueNot1 !== valueNot2;
+}
+
+function addOne(numberAdd) {
+    return numberAdd = (numberAdd) + 1;
+}
+
+function isEven(numberEven) {
+    if (numberEven % 2 == 0)
+        return true;
+    else
+        return false;
+}
+
+function isIdentical(identical1, identical2) {
+    return identical1 === identical2;
+}
+
+function isEqual(equal1, equal2) {
+    return equal1 == equal2;
+}
+
+function or(option1, option2) {
+    return option1 || option2;
+}
+
+function and(and1, and2) {
+    return and1 && and2;
+}
+
+function concat(lineA, lineB) {
+    return (lineA.concat(lineB));
+}
